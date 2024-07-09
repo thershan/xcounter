02666d1231a185b0138f4ba6c598af6398d0b446
lonelywolf757 <lonelywolf757@gmail.com>
2024-07-09 18:33:09 +0530

    Initial commit with class-based Counter component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import Counter from './Counter';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'; // Assuming you have some basic styles
 
-const App = () => {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
-};
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
 
-export default App;
+  increment = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  };
+
+  decrement = () => {
+    this.setState(prevState => ({
+      count: prevState.count - 1
+    }));
+  };
+
+  render() {
+    return (
+      <div className="counter-container">
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
